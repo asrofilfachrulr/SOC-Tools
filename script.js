494172c7ad3c891585b11b439d433d2449449fb9
asrofil <riidloa@gmail.com>
2024-10-10 14:29:26 +0700

    add defang and rephrase

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function clearIPExt(){
     document.getElementById("ib-output-ip-ext").value = ""
 }
 
-function parse(){
+function extract(){
     const ipv4Regex = /(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/g;
 
     text = document.getElementById("ta-rawtext-ip-ext").value
@@ -17,5 +17,28 @@ function parse(){
 
 function copyIPExt(){
     const text = document.getElementById("ib-output-ip-ext").value
+    navigator.clipboard.writeText(text)
+}
+
+// Defang URL Functions
+
+function clearDefang(){
+    document.getElementById("ib-rawtext-url-defang").value = ""
+    document.getElementById("ib-output-url-defang").value = ""
+}
+
+function defang(){
+    text = document.getElementById("ib-rawtext-url-defang").value
+
+    const defang = text.replace(/:/g, '[:]')
+                        .replace(/\./g, '[.]')
+                        .replace(/\//g, '[/]');
+
+
+    document.getElementById("ib-output-url-defang").value = defang
+}
+
+function copyDefang(){
+    const text = document.getElementById("ib-output-url-defang").value
     navigator.clipboard.writeText(text)
 }
